f90aa4a37b695e3a613d51fa2e95bb68346142a7
Cooper Edmunds <cooper6789@gmail.com>
2018-12-19 23:30:24 -0500

    Set basic skeleton

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import MainPage from "./MainPage";
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <MainPage />
+      </div>
+    );
   }
 }
 
