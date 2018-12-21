9735ab2282a28c7f60e97f5cf0d9bc567ea2a876
nicole.moiseyev3400@gmail.com <nicole.moiseyev3400@gmail.com>
2018-12-20 21:40:59 -0500

    Edit data, add image and arrows

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -36,11 +36,16 @@ export default class HomePage extends Component {
   render() {
     var graph = {
       nodes: [
-        { id: 1, label: "Node 1" },
-        { id: 2, label: "Node 2" },
-        { id: 3, label: "Node 3" },
-        { id: 4, label: "Node 4" },
-        { id: 5, label: "Node 5" }
+        {
+          id: 1,
+          label: "Duke",
+          image:
+            "https://s3-us-west-2.amazonaws.com/static.fantasydata.com/logos/ncaa/26.png"
+        },
+        { id: 2, label: "UNC" },
+        { id: 3, label: "Gonzaga" },
+        { id: 4, label: "GADU University" },
+        { id: 5, label: "Clark Bray" }
       ],
       edges: [
         { from: 1, to: 2 },
